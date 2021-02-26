18da9a9565c8d66cec86a3806cbde95ba945e0ae
Adrian Ross <79227951+airtableandchill@users.noreply.github.com>
2021-02-26 15:03:24 -0500

    Validate names
    Validate the generated name and provide an alternative way to get an employee's name. Drafted error handling

--- assignEmployeeRecord.js
+++ assignEmployeeRecord.js
@@ -1,21 +1,24 @@
 // Grab all employees that are working today from the "Today's Shifts" view of the Shifts table.
 const shiftsTable = base.getTable("Shifts");
 const todaysShiftsView = shiftsTable.getView("Today's Shifts");
-const todaysShiftsQuery = await todaysShiftsView.selectRecordsAsync({ fields: ["Shift", "Name"] });
+const todaysShiftsQuery = await todaysShiftsView.selectRecordsAsync({ fields: ["Shift", "Name", "Employee Record"] });
 
 // Get a list of employee names (and record IDs!) for everyone working today.
 let todaysShiftsRecords = todaysShiftsQuery.records.map(record => {
-    const shiftId = record.id || null;
+    const shiftID = record.id || null;
 
-    /*
-    WARNING: This is flimsy code!
-    If the "Date of Shift" field in the "Shifts by Date" view for the "Schedule Changes" table of the Attendance Beta is modified in any way, this line below will fail and subsequent steps won't work.
-    Alternatively, we can uncomment the name2 variable to literally grab employee names from the "Name field".
-    */
+    // generatedName grabs an employee's name from the "Shift" field in the in the "Today's Shifts" view.
+    const generatedName = record.name.split("-")[0].trim() || "";
+    const nameRegex = new RegExp(/^[a-zA-Z\s]*$/);
+    const validName = generatedName && generatedName.split(" ").length === 2 && nameRegex.test(generatedName);
 
-    const name = record.name.split("-")[0].trim() || "";
-    // const name2 = shiftsTodayQuery.getRecord(record.id).getCellValue("Name");
-    return { name, shiftId, employeeID: undefined }
+    if (validName) {
+        return { name: generatedName, shiftID, employeeID: undefined };
+    } else {
+        // If the generatedName isn't valid, we'll just grab the employee's name from their record instead.
+        const employeeName = todaysShiftsQuery.getRecord(record.id).getCellValueAsString("Name");
+        return { name: employeeName, shiftID, employeeID: undefined };
+    }
 });
 
 const employeesTable = base.getTable("Employees");
@@ -23,6 +26,7 @@ const allEmployeesView = employeesTable.getView("All employees");
 const allEmployeesQuery = await allEmployeesView.selectRecordsAsync({ fields: ["Name"] });
 const allEmployeeRecords = allEmployeesQuery.records;
 
+// For each employee working today, find their employee record ID.
 todaysShiftsRecords.map(shiftRecord => {
     const employeeRecord = allEmployeeRecords.find(employeeRecord => employeeRecord.name === shiftRecord.name);
 
@@ -31,8 +35,21 @@ todaysShiftsRecords.map(shiftRecord => {
     }
 });
 
-// Now we'll update 
+// console.log(todaysShiftsRecords);
 
+// Now we use the employee record ID as the value for the "Employee Record" field in the "Today's Shifts" view.
 todaysShiftsRecords.forEach(record => {
-    shiftsTable.updateRecordAsync(record.shiftId, {"Employee Record": [{id: record.employeeID}]});
+    const hasEmployeeRecord = todaysShiftsQuery.getRecord(record.shiftID).getCellValueAsString("Employee Record") !== "Unnamed record";
+
+    if (hasEmployeeRecord) {
+        // console.log(`Skipping ${record.name}`)
+        return; // If the shift already has an employee record assigned, skip it!
+    } else {
+        // console.log(`Updating record: ${record.name}`)
+        try {
+            shiftsTable.updateRecordAsync(record.shiftID, { "Employee Record": [{ id: record.employeeID }] });
+        } catch (error) {
+            console.error(error); // We console log this for now, but eventually we will log this error by adding a record to a new table.
+        }
+    }
 });
