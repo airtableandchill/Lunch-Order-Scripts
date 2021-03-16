9b2324dd5e7af27a487257c521f3b7194ea5b9d4
Adrian Ross <79227951+airtableandchill@users.noreply.github.com>
2021-03-16 15:12:31 -0400

    Log error when no employees are found
    Add an if statement to prevent last half of script from running if no employees are found, log an error if no employees are found, console.log employee records and how many records there are for convenience when checking script Runs

--- assignEmployeeRecord.js
+++ assignEmployeeRecord.js
@@ -5,23 +5,27 @@ const shiftsTable = base.getTable("Shifts");
 const todaysShiftsView = shiftsTable.getView("Today's Shifts");
 const todaysShiftsQuery = await todaysShiftsView.selectRecordsAsync({ fields: ["Shift", "Name", "Employee Record"] });
 
-// Get a list of employee names (and record IDs!) for everyone working today.
-let todaysShiftsRecords = todaysShiftsQuery.records.map(record => {
-    const shiftID = record.id || null;
+function getEmployeeRecords() {
+    // Get a list of employee names (and record IDs!) for everyone working today.
+    return todaysShiftsQuery.records.map(record => {
+        const shiftID = record.id || null;
 
-    // generatedName grabs an employee's name from the "Shift" field in the in the "Today's Shifts" view.
-    const generatedName = record.name.split("-")[0].trim() || "";
-    const nameRegex = new RegExp(/^[a-zA-Z\s]*$/);
-    const validName = generatedName && generatedName.split(" ").length === 2 && nameRegex.test(generatedName);
+        // generatedName grabs an employee's name from the "Shift" field in the in the "Today's Shifts" view.
+        const generatedName = record.name.split("-")[0].trim() || "";
+        const nameRegex = new RegExp(/^[a-zA-Z\s]*$/);
+        const validName = generatedName && generatedName.split(" ").length === 2 && nameRegex.test(generatedName);
 
-    if (validName) {
-        return { name: generatedName, shiftID, employeeID: undefined };
-    } else {
-        // If the generatedName isn't valid, we'll just grab the employee's name from their record instead.
-        const employeeName = todaysShiftsQuery.getRecord(record.id).getCellValueAsString("Name");
-        return { name: employeeName, shiftID, employeeID: undefined };
-    }
-});
+        if (validName) {
+            return { name: generatedName, shiftID, employeeID: undefined };
+        } else {
+            // If the generatedName isn't valid, we'll just grab the employee's name from their record instead.
+            const employeeName = todaysShiftsQuery.getRecord(record.id).getCellValueAsString("Name");
+            return { name: employeeName, shiftID, employeeID: undefined };
+        }
+    });
+}
+
+let todaysShiftsRecords = getEmployeeRecords();
 
 const employeesTable = base.getTable("Employees");
 const allEmployeesView = employeesTable.getView("All employees");
@@ -37,27 +41,33 @@ todaysShiftsRecords.map(shiftRecord => {
     }
 });
 
-// console.log(todaysShiftsRecords);
+console.log("Employee Records", todaysShiftsRecords);
+console.log(`Total employee records: ${todaysShiftsRecords.length}`);
 
-// Now we use the employee record ID as the value for the "Employee Record" field in the "Today's Shifts" view.
-todaysShiftsRecords.forEach(record => {
-    let hasEmployeeRecord = true;
+if (todaysShiftsRecords.length) {
 
-    try {
-        hasEmployeeRecord = todaysShiftsQuery.getRecord(record.shiftID).getCellValueAsString("Employee Record") !== "";
-    } catch (error) {
-        errorsTable.createRecordAsync({ "Script Name": "assignEmployeeRecords", Message: error.message, Type: error.name, Stack: error.stack });
-    };
+    // Now we use the employee record ID as the value for the "Employee Record" field in the "Today's Shifts" view.
+    todaysShiftsRecords.forEach(record => {
+        let hasEmployeeRecord = true;
 
-    if (hasEmployeeRecord) {
-        // console.log(`Skipping ${record.name}`)
-        return; // If the shift already has an employee record assigned, skip it!
-    } else {
-        // console.log(`Updating record: ${record.name}`)
         try {
-            shiftsTable.updateRecordAsync(record.shiftID, { "Employee Record": [{ id: record.employeeID }] });
+            hasEmployeeRecord = todaysShiftsQuery.getRecord(record.shiftID).getCellValueAsString("Employee Record") !== "";
         } catch (error) {
             errorsTable.createRecordAsync({ "Script Name": "assignEmployeeRecords", Message: error.message, Type: error.name, Stack: error.stack });
+        };
+
+        if (hasEmployeeRecord) {
+            // console.log(`Skipping ${record.name}`)
+            return; // If the shift already has an employee record assigned, skip it!
+        } else {
+            // console.log(`Updating record: ${record.name}`)
+            try {
+                shiftsTable.updateRecordAsync(record.shiftID, { "Employee Record": [{ id: record.employeeID }] });
+            } catch (error) {
+                errorsTable.createRecordAsync({ "Script Name": "assignEmployeeRecords", Message: error.message, Type: error.name, Stack: error.stack });
+            }
         }
-    }
-});
+    });
+} else {
+    errorsTable.createRecordAsync({ "Script Name": "assignEmployeeRecords", Message: "No employees found." });
+}
